63d876aa96bd51a010f400f21cce03395b3a734a
FettacheRachik <47011772+FettacheRachik@users.noreply.github.com>
2019-02-11 20:46:52 +0100

    Add files via upload

--- langstat.sh
+++ langstat.sh
@@ -33,16 +33,16 @@ if [ $# -gt 2 ];then
 exit
 fi
 
-#Recuperer valeur $1 affecteur a la variable fichier
+#Recuperer valeur $1 affecter a variable fichier
 fichier="$1"
+#Compteur utilisé
 let "compteur = 0 "
-#Invite a ressaisir le chemin du fichier
-#Si parametre est vide , c est un dossier , fichier vide , repertoire
+#Invite a ressaisir le chemin du fichier :Si parametre est vide , c est un dossier , fichier vide , repertoire inaccessible en lecture
 while [ -z "$fichier" -o ! -e "$fichier" -o -d "$fichier" -o ! -s "$fichier" -o ! -r "$fichier" ]
 do
 #on va verifier si le fichier dico.txt existe dans le repertoire courant
 trouveDico=$( find -name dico.txt | wc -l )
-
+#Le compteur servira a afficher une seul fois le chemin du fichier
 if [ $trouveDico -ge 1 ] && [ "$compteur" = "0" ]
 then
 echo "Un fichier dico.txt a été trouvé dans le repertoire courant: "
@@ -66,33 +66,47 @@ fi
 read -p 'Merci d indiquer le chemin du fichier à traiter : ' fichier
 done
 
-#Creation du fichier et du tableau contenant l alphabet
->statLettres.txt
+#Creation du fichier contenant les statistiques et du tableau contenant l alphabet
+echo ''>statLettres.txt
+
 alphabet=('A' 'B' 'C' 'D' 'E' 'F' 'G' 'H' 'I' 'J' 'K' 'L' 'M' 'N' 'O' 'P' 'Q' 'R' 'S' 'T' 'U' 'V' 'W' 'X' 'Y' 'Z')
 
+#Parcours de chaque lettre composant le tableau
 for letter in ${alphabet[@]}
 do
-#substituion de commande pour recuperer le nombre de fois ou la lettre apparait dans un mot
+#Recuperation du  nombre de fois ou la lettre apparait 
 occurences=$(grep -i $letter $fichier | wc -l)
 
-#On inscrit le resultat dans le fichier statLettres.txt
-echo  "$occurences - $letter" >>statLettres.txt
+#On inscrit le resultat dans le fichier statLettres.txt >>
+echo  "$occurences    -    $letter" >>statLettres.txt
 done
 
-#Affichage du resultat et Tri du fichier statLettres.txt
-echo -e "\nTri decroissant de la présence des lettres dans les mots du dictionnaire"
+#Affichage du resultat et Tri du fichier statLettres.txt , puis suppression du fichier
+echo -e "\nSTATISTIQUES LETTRES DANS LE DICTIONNAIRE : "
 sort -nr statLettres.txt 
+rm statLettres.txt
+
 #Fonction rercherche
 recherche(){
 read -p 'Tappez le mot que vous rercherchez : ' mot
+	#On verifie avec expression reguliere si le mot recherché est trouvé dans le dictionnaire
         nbreMotTrouve=$(grep -Ei ^$mot$ $fichier | wc -l)
+
+	#On verifie si il existe des mots  commencant par le mot recherché
         nbreMotDebut=$(grep -Ei ^$mot $fichier | wc -l)
         if [ $nbreMotTrouve -ge 1 ];then
-            echo "Mot(s) trouvé(s) : "
-            grep -Ein $mot $fichier  
+            echo "Mot trouvé  : "
+            grep -Ein ^$mot$ $fichier
+
+	#On ecrit dans le fichier motsDebut.txt les mots débutant par le mot recherche
+        #On inscrit le numéro de ligne (-n) et le mot trouvé
+	#On affiche avec "less"
         elif [ $nbreMotDebut -ge 1 ] ;then
-            echo "Il y a $nbreMotDebut  mot(s) débutant par $mot : "
-            grep -Ein ^$mot $fichier
+	    echo ''>motsDebut.txt
+            echo "Il y a $nbreMotDebut  mot(s) débutant par $mot : ">>motsDebut.txt
+            grep -Ein ^$mot $fichier >>motsDebut.txt
+	    less motsDebut.txt
+	    rm motsDebut.txt
         else
             echo -e "\nAucune correspondance a été trouvée "
         fi
